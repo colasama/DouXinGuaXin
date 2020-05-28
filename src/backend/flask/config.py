94096b9238f53b0e8b298afea5a4ee744b6b0867
Fewing <yangzheyu98@gmail.com>
2020-05-28 19:20:59 +0800

    更新私钥

--- src/backend/flask/config.py
+++ src/backend/flask/config.py
@@ -4,7 +4,7 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '\xc9ixnRb\xe40\xd4\xa5\x7f\x03\xd0y6\x01\x1f\x96\xeao+\x8a\x9f\xe4'
+    SECRET_KEY = '\xc9ixnRb\xe40\xd4\xa6\x7f\x03\xd0y6\x02\x1f\x96\xeao+\x8a\x9f\xe4'
     TOKEN_EXPIRATION = 3600
 
 
