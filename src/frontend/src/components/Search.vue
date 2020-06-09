2760391ca3afa006aafa71a01fa01fd17ec41d28
DbettKK <394739138@qq.com>
2020-06-09 19:00:46 +0800

    搜索相关交互上传

--- src/frontend/src/components/Search.vue
+++ src/frontend/src/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="text-align:center">
-        <a-select style="width: 70px" default-value="书籍">
+        <a-select v-model="value" style="width: 70px">
           <a-select-option value="book">
             书籍
           </a-select-option>
@@ -13,13 +13,36 @@
           <a-select-option value="topic">
             话题
           </a-select-option>
-          <a-select-option value="pictext">
+          <a-select-option value="topic_content">
             图文
           </a-select-option>
-          <a-select-option value="post">
+          <a-select-option value="group_content">
             帖子
           </a-select-option>
         </a-select>
-        <a-input-search placeholder="搜索" style="width: 200px" @search="onSearch"/>
+        <a-input-search placeholder="搜索" style="width: 200px" @search="onSearch" v-model="keywords"/>
     </div>
 </template>
+<script>
+    import Vue from "vue";
+    export default{
+        data() {
+            return{
+                value:"book",
+                keywords:""
+            }
+        },
+        methods:{
+            onSearch(){
+                Vue.axios.get('http://182.92.57.178:5000/search/' + this.value + 's', {
+                    params:{keywords:this.keywords}
+                }).then(res=>{
+                    console.log(res)    //后续处理
+                }).catch(function(error){
+                    console.log(error);
+                })
+                console.log(this.value) //可删
+            }
+        }
+    }
+</script>
