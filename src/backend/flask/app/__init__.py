0ca1d1f8ffb48be3bde950a9da0791b4fc4bc1da
Fewing <yangzheyu98@gmail.com>
2020-06-09 18:56:23 +0800

    前端：书籍详情

--- src/backend/flask/app/__init__.py
+++ src/backend/flask/app/__init__.py
@@ -5,9 +5,10 @@ from flask_restful import Api
 from config import Config
 from flask_mail import Mail
 from flask_apscheduler import APScheduler
-
+from flask_cors import *
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 api = Api(app)
 app.config.from_object(Config)
 mail = Mail(app)
--- src/frontend/src/views/Book/Object.vue
+++ src/frontend/src/views/Book/Object.vue
@@ -1,143 +1,160 @@
 <template>
   <div>
-  {{this.$route.params.id}}
-  <a-layout style="min-height:100%">
+    {{this.$route.params.id}}
+    <a-layout style="min-height:100%">
       <a-layout-content style="margin-top:30px;text-align:center">
-        
         <a-layout>
-            <a-layout-content>
-                <a-page-header
-                    style="margin-left:0"
-                    title="返回上一页"
-                    @back="back"
-                />
-                <a-card style="margin:0 20px 0 20px">
-                
-                <a-row>
-                    <img style="text-align:left" :src="book[0].coversrc" height="500px"  />
-                </a-row>
-                <a-row>
-                <h style="font-size:40px">{{book[0].name}}</h>
-                <div style="font-size:18px">{{book[0].author}}</div>
-                </a-row>
-                <a-row>
-                    <a-rate :default-value="book[0].star" disabled /> 
-                </a-row>
-                <a-row>
-                    <div style="font-size:32px"><h1>{{book[0].star}}</h1></div>
-                </a-row>
-                <a-row>  
-                    <div style="margin:0 50px 0 50px">{{book[0].pro}}</div>
-                </a-row>
-                </a-card>
-            </a-layout-content>
+          <a-layout-content>
+            <a-page-header style="margin-left:0" title="返回上一页" @back="back" />
+            <a-card style="margin:0 20px 0 20px">
+              <a-row>
+                <img style="text-align:left" :src="book[0].coversrc" height="500px" />
+              </a-row>
+              <a-row>
+                <h style="font-size:40px">{{info.Book_name}}</h>
+                <div style="font-size:18px">{{info.Book_writer}}</div>
+              </a-row>
+              <a-row>
+                <a-rate :value="info.Book_score/2" disabled />
+              </a-row>
+              <a-row>
+                <div style="font-size:32px">
+                  <h1>{{info.Book_score}}</h1>
+                </div>
+              </a-row>
+              <a-row>
+                <div style="margin:0 50px 0 50px">{{info.Book_intro}}</div>
+              </a-row>
+            </a-card>
+          </a-layout-content>
         </a-layout>
-        <a-page-header
-            style="margin-left:10%;margin-top:30px"
-            title="评论列表"
-            sub-title="Comments"
-        />
+        <a-page-header style="margin-left:10%;margin-top:30px" title="评论列表" sub-title="Comments" />
         <a-row type="flex" justify="space-around" align="middle">
-        <a-col :span="24">
+          <a-col :span="24">
+            <div>
+              <a-avatar icon="user" />
+              <a-avatar>
+                <a-icon slot="icon" type="user" />
+              </a-avatar>
+              <a-avatar>U</a-avatar>
+              <a-avatar>USER</a-avatar>
+              <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              <a-avatar style="color: #f56a00; backgroundColor: #fde3cf">U</a-avatar>
+              <a-avatar style="backgroundColor:#87d068" icon="user" />
+            </div>
 
-        
-        <div>
-            <a-avatar icon="user" />
-            <a-avatar>
-            <a-icon slot="icon" type="user" />
-            </a-avatar>
-            <a-avatar>U</a-avatar>
-            <a-avatar>USER</a-avatar>
-            <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-            <a-avatar style="color: #f56a00; backgroundColor: #fde3cf">
-            U
-            </a-avatar>
-            <a-avatar style="backgroundColor:#87d068" icon="user" />
-        </div>
-        
-        <!--以下是列表渲染部分-->
-        <a-list
-            class="comment-list"
-            :header="`共有${data.length}条评论`"
-            item-layout="vertical"
-            :data-source="data"
-            style="margin:20px;text-align:center"
-        >
-          <a-list-item slot="renderItem" slot-scope="item" >
-            <a-comment :author="item.author" :avatar="item.avatar">
-                <template slot="actions" >
-                <span> <a-icon type="like-o" style="margin-left: 8px" /> 赞</span>
-                <span> <a-icon type="dislike-o" style="margin-left: 8px" /> 踩</span>
-                <span> <a-icon type="message" style="margin-left: 8px" /> 回复</span>
-                <span> <a-icon type="warning" style="margin-left: 8px" /> 举报</span>
-                </template>
-                <p slot="content">
-                {{ item.content }}
-                </p>
-                <a-tooltip slot="datetime" :title="item.datetime.format('YYYY-MM-DD HH:mm:ss')">
-                <span>{{ item.datetime.fromNow() }}</span>
-                </a-tooltip>
-            </a-comment>
-          </a-list-item>
-        </a-list>
+            <!--以下是列表渲染部分-->
+            <a-list
+              class="comment-list"
+              :header="`共有${comments.length}条评论`"
+              item-layout="vertical"
+              :data-source="comments"
+              style="margin:20px;text-align:center"
+            >
+              <a-list-item slot="renderItem" slot-scope="item">
+                <a-comment :author="item.User_id" :avatar="item.User_id">
+                  <template slot="actions">
+                    <span>
+                      <a-icon type="like-o" style="margin-left: 8px" />赞
+                    </span>
+                    <span>
+                      <a-icon type="dislike-o" style="margin-left: 8px" />踩
+                    </span>
+                    <span>
+                      <a-icon type="message" style="margin-left: 8px" />回复
+                    </span>
+                    <span>
+                      <a-icon type="warning" style="margin-left: 8px" />举报
+                    </span>
+                  </template>
+                  <p slot="content">{{ item.Book_comment_content }}</p>
+                  <a-tooltip slot="datetime" :title="item.Create_time">
+                    <span>{{ item.Create_time}}</span>
+                  </a-tooltip>
+                </a-comment>
+              </a-list-item>
+            </a-list>
 
-        <div style="margin:0 auto;max-width:1000px">
-        <a-card title="发表评论" style="text-align:center;margin:24px">
-            <div style="font-size:30px" v-if="commentRate">{{commentRate}}</div>
-            <a-rate v-model="commentRate" allow-half />
-            <a-textarea 
-            v-model="commentValue"
-            placeholder="请输入评论内容"
-            :auto-size="{ minRows: 4, maxRows: 4 }"
-            style="margin:14px 5px 0 5px;"
-            />
-            <a-button style="margin:15px 5px 0 5px;">发表</a-button>
-        </a-card>
-        </div>
-        </a-col>
-       </a-row> 
+            <div style="margin:0 auto;max-width:1000px">
+              <a-card title="发表评论" style="text-align:center;margin:24px">
+                <div style="font-size:30px" v-if="commentRate">{{commentRate}}</div>
+                <a-rate v-model="commentRate" allow-half />
+                <a-textarea
+                  v-model="commentValue"
+                  placeholder="请输入评论内容"
+                  :auto-size="{ minRows: 4, maxRows: 4 }"
+                  style="margin:14px 5px 0 5px;"
+                />
+                <a-button style="margin:15px 5px 0 5px;">发表</a-button>
+              </a-card>
+            </div>
+          </a-col>
+        </a-row>
       </a-layout-content>
     </a-layout>
   </div>
 </template>
 
 <script>
-import moment from 'moment';
+import moment from "moment";
 
 export default {
-    data() {
-        return {
-        commentRate: 0,
-        book: [
-            {name:'Hunter X Hunter', 
-            author:'富坚义博', 
-            pro:'故事讲述一个自幼丧母的少年小冈，为了要寻找失散多年的父亲，及成为一个和父亲一样出色的“猎人”，踏上了崎岖而漫长的旅程，接受重重测试，途中遇上各色各样的同伴和敌人，各自为了不同的目的而展开一场又一场的战斗……',
-            star:'5',
-            coversrc:"https://img9.doubanio.com/view/subject/l/public/s26041185.jpg"},
-            
-        ],
-        data: [
-            {
-            author: 'Han Solo',
-            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-            content:
-                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-            datetime: moment().subtract(1, 'days'),
-            },
-            {
-            author: 'Han Solo',
-            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-            content:
-                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-            datetime: moment().subtract(2, 'days'),
-            }],
-            moment,
+  data() {
+    return {
+      commentRate: 0,
+      info:{},
+      comments:{},
+      book: [
+        {
+          name: "Hunter X Hunter",
+          author: "富坚义博",
+          pro:
+            "故事讲述一个自幼丧母的少年小冈，为了要寻找失散多年的父亲，及成为一个和父亲一样出色的“猎人”，踏上了崎岖而漫长的旅程，接受重重测试，途中遇上各色各样的同伴和敌人，各自为了不同的目的而展开一场又一场的战斗……",
+          star: "5",
+          coversrc:
+            "https://img9.doubanio.com/view/subject/l/public/s26041185.jpg"
         }
-    },
+      ],
+      data: [
+        {
+          author: "Han Solo",
+          avatar:
+            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+          content:
+            "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+          datetime: moment().subtract(1, "days")
+        },
+        {
+          author: "Han Solo",
+          avatar:
+            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+          content:
+            "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+          datetime: moment().subtract(2, "days")
+        }
+      ],
+      moment
+    };
+  },
+  mounted: function() {
+    console.log("乌乌创建了");
+    console.log(this.$route.params.id.substr(1));
+    this.$http
+      .get("http://182.92.57.178:5000/books/"+this.$route.params.id.substr(1))
+      .then(response => {
+        this.info = response.data.result.info;
+        this.comments = response.data.result.comments;
+        console.log(this.info);
+        console.log(this.comments.length);
+      })
+      .catch(response => {
+        console.log(response);
+      });
+  },
   methods: {
-    back(){
-        this.$router.push({path:"/book/index"});
-    },
+    back() {
+      this.$router.push({ path: "/book/index" });
+    }
   }
 };
 </script>
