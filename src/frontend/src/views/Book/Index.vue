7bc37ab2334a86c4ded1dbf6ddf4325044acc52d
Colanns <mycolands@gmail.com>
2020-06-12 21:15:00 +0800

    修改了书籍首页

--- src/frontend/src/views/Book/Index.vue
+++ src/frontend/src/views/Book/Index.vue
@@ -7,12 +7,12 @@
             title="书籍"
             sub-title="Books"
         />
-        <a-button @click="refresh">Refresh</a-button>
-        <div>{{books}}</div>
+        <!--a-button @click="refresh">Refresh</a-button>
+        <div>{{books}}</div-->
         <!--以下是列表渲染部分-->
-        <a-list :grid="{ gutter: 16, column: 4 }" :data-source="books.result" style="margin:24px">
+        <a-list :grid="{ gutter: 16, column: 4 }" :data-source="books.result" style="margin:24px;text-align:center">
           <a-list-item slot="renderItem" slot-scope="book">
-            <a-card style="max-width:400px;min-width:400px">
+            <a-card style="max-width:400px;min-width:400px;max-height:240px;min-height:240px">
               <a-row type="flex" justify="center" align="center">
                 <a-col :span="8" style="text-align:left" >
                 <a :href="'/#/book/object/'+book.Book_id">
@@ -27,7 +27,7 @@
                   <div><a :href="'/#/book/object/'+book.Book_id" style="font-size:20px;"><b>{{book.Book_name}}</b></a></div>
                   <!--a-card-meta :title="book.Book_name"/-->
                     <div style="margin-top:10px;text-indent:2em;text-align:justify">{{book.Book_intro.substring(0,50)}}...</div>
-                  <a-rate style="margin-top:10px" :default-value="book.Book_score/2" disabled />
+                  <a-rate style="margin-bottom:10px" :default-value="book.Book_score/2" disabled />
                 </a-col>
               </a-row>
             </a-card>
