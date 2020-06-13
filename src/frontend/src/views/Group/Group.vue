f132525f8f5192c30a2a5bc0474cf74720347c83
Fewing <yangzheyu98@gmail.com>
2020-06-13 17:05:52 +0800

    完成发表帖子

--- src/frontend/src/views/Group/Group.vue
+++ src/frontend/src/views/Group/Group.vue
@@ -16,7 +16,7 @@
             >
               <b>加入小组</b>
             </a-button>
-            <a-button v-if="ifJoinedGroup&&ifLoggedIn" type="default">
+            <a-button v-if="ifJoinedGroup&&ifLoggedIn" type="default" @click="showPost">
               <b>发表帖子</b>
             </a-button>
             <div style="margin-top:50px">
@@ -33,50 +33,60 @@
         </div>
 
         <!--发帖框，由postVisible控制-->
-            <a-modal  
-                centered="true"
-                title="发表帖子"
-                :visible="postVisible"
-                :confirm-loading="confirmLoading"
-                @ok="handleOk"
-                @cancel="handleCancel"
-                okText="发表"
-                cancelText="取消"
-                >
-                <div>
-                        <a-input style="margin-top:10px" placeholder="请填写帖子标题" />
-                        <a-textarea style="margin-top:10px" placeholder="请填写帖子内容，要大于25字哦~" :auto-size="{ minRows: 5, maxRows: 5 }"></a-textarea>
-                        <!--a-button style="margin-top:10px">提交</a-button-->
-                </div>
-            </a-modal>
+        <a-modal
+          centered="true"
+          title="发表帖子"
+          :visible="postVisible"
+          :confirm-loading="confirmLoading"
+          @ok="handleOk"
+          @cancel="handleCancel"
+          okText="发表"
+          cancelText="取消"
+        >
+          <div>
+            <a-input v-model="postTitle" style="margin-top:10px" placeholder="请填写帖子标题" />
+            <a-textarea
+              style="margin-top:10px"
+              v-model="postContent"
+              placeholder="请填写帖子内容，要大于25字哦~"
+              :auto-size="{ minRows: 5, maxRows: 5 }"
+            ></a-textarea>
+            <!--a-button style="margin-top:10px">提交</a-button-->
+          </div>
+        </a-modal>
 
-            <a-divider>
-                <div style="font-size:18px">共有{{posts.length}}条帖子</div>
-            </a-divider>
+        <a-divider>
+          <div style="font-size:18px">共有{{posts.length}}条帖子</div>
+        </a-divider>
 
         <!--帖子渲染部分-->
         <a-list
-                class="comment-list"
-                item-layout="vertical"
-                :data-source="posts"
-                style="margin:24px;text-align:center"
-            >
-            <a-list-item slot="renderItem" slot-scope="item" style="text-align:left">
-                <a-list-item-meta :description="item.Group_content_content">
-                  <a-avatar slot="avatar">{{item.User_name.substring(0,1)}}</a-avatar>
-                    <a slot="title" :href="'/object/'+item.Group_content_id">
-                    {{item.Group_content_title}}
-                    <a-tag style="margin-left:8px" color="red">精华</a-tag>
-                    <a-tag style="margin-left:8px" color="orange">置顶</a-tag>
-                    </a>
-                    <!--a-avatar slot="avatar" :src="item.avatar" /-->
-                </a-list-item-meta>
-                <template slot="actions" >
-                    <span> <a-icon type="user" style="margin-left: 8px" /> {{item.User_name}}</span>
-                    <a-tooltip :title="item.Create_time"><span>{{ item.Create_time}}</span></a-tooltip>    
-                </template>
-            </a-list-item>
-            </a-list>
+          class="comment-list"
+          item-layout="vertical"
+          :data-source="posts"
+          style="margin:24px;text-align:center"
+        >
+          <a-list-item slot="renderItem" slot-scope="item" style="text-align:left">
+            <a-list-item-meta :description="item.Group_content_content">
+              <a-avatar slot="avatar">{{item.User_name.substring(0,1)}}</a-avatar>
+              <a slot="title" :href="'/object/'+item.Group_content_id">
+                {{item.Group_content_title}}
+                <a-tag style="margin-left:8px" color="red" v-if="item.Is_highlighted">精华</a-tag>
+                <a-tag style="margin-left:8px" color="orange" v-if="item.Is_pinned">置顶</a-tag>
+              </a>
+              <!--a-avatar slot="avatar" :src="item.avatar" /-->
+            </a-list-item-meta>
+            <template slot="actions">
+              <span>
+                <a-icon type="user" style="margin-left: 8px" />
+                {{item.User_name}}
+              </span>
+              <a-tooltip :title="item.Create_time">
+                <span>{{ item.Create_time}}</span>
+              </a-tooltip>
+            </template>
+          </a-list-item>
+        </a-list>
       </a-layout-content>
     </a-layout>
   </div>
@@ -99,61 +109,40 @@ export default {
   components: {},
   data() {
     return {
-      posts: [
-        {
-          Group_content_id: 4,
-          Group_content_content: "testtesttest",
-          Group_content_title: "test",
-          Group_id: 1,
-          User_id: 15,
-          Username: "test",
-          Group_content_image: "/example",
-          Create_time: "2020-06-03 17:50:52",
-          Is_highlighted: 1,
-          Is_pinned: 1
-        },
-        {
-          Group_content_id: 5,
-          Group_content_content: "testtesttest123",
-          Group_content_title: "test123",
-          Group_id: 1,
-          User_id: 15,
-          Username: "test",
-          Group_content_image: "/example",
-          Create_time: "2020-06-03 18:30:04",
-          Is_highlighted: 1,
-          Is_pinned: 1
-        }
-      ],
-      postVisible:true,
-      ifJoinedGroup:false,
-      ifLoggedIn:false,
-      id:-1,
-      info:[],
+      posts: [],
+      postVisible: false,
+      ifJoinedGroup: false,
+      ifLoggedIn: false,
+      id: -1,
+      info: [],
+      postTitle: "",
+      postContent: ""
     };
   },
   mounted: function() {
     this.ifLoggedIn = global_.loginStatus;
     this.id = this.$route.params.id;
     console.log(this.id);
-    this.$http
-      .get("http://182.92.57.178:5000/groups/" + this.$route.params.id)
-      .then(response => {
-        this.info = response.data.result.info;
-        this.posts = response.data.result.contents;
-        console.log(this.info);
-      })
-      .catch(response => {
-        console.log(response);
-      });
-    global_.my_groups.forEach(element => {
-      if (element.Group_id == this.$route.params.id) {
-        this.ifJoinedGroup = true;
-      }
-    });
+    this.load_data(this.id);
   },
   methods: {
-    //估计得有一个getUsernameByID一个getPosts
+    load_data() {
+      this.$http
+        .get("http://182.92.57.178:5000/groups/" + this.$route.params.id)
+        .then(response => {
+          this.info = response.data.result.info;
+          this.posts = response.data.result.contents;
+          console.log(this.info);
+        })
+        .catch(response => {
+          console.log(response);
+        });
+      global_.my_groups.forEach(element => {
+        if (element.Group_id == this.$route.params.id) {
+          this.ifJoinedGroup = true;
+        }
+      });
+    },
     joingroup() {
       if (global_.loginStatus == false) {
         alert("请先登录");
@@ -182,34 +171,66 @@ export default {
           console.log(res);
         });
     },
+    showPost() {
+      this.postVisible = true;
+    },
     //发表帖子的函数们
     handleChange({ fileList }) {
       this.fileList = fileList;
     },
-    showReport(){
-      this.postVisible=true;
-      },
-    handleOk(e){
+    showReport() {
+      this.postVisible = true;
+    },
+    handleOk(e) {
       console.log(e);
-      this.postVisible=false;
-      this.showSuccess();
+      console.log(this.postTitle);
+      console.log(this.postContent);
+      if (this.postContent.length < 25) {
+        alert("帖子内容应不少于25字");
+        return;
+      }
+      Vue.axios
+        .post(
+          "http://182.92.57.178:5000/groups/" +
+            this.$route.params.id +
+            "/add_content",
+          {
+            group_content_title: this.postTitle,
+            group_content_content: this.postContent
+          },
+          {
+            headers: {
+              token: global_.token
+            }
+          }
+        )
+        .then(response => {
+          console.log(response);
+          this.load_data();
+          this.postVisible = false;
+          this.showSuccess();
+        })
+        .catch(response => {
+          console.log(response);
+          alert("发帖失败");
+        });
     },
-    handleCancel(){
-                this.postVisible=false;
-            },
-    destroyALL(){
-                this.$destroyAll();
-            },
-    showSuccess(){
-                this.$success({
-                    centered: true,
-                    title: '发表成功',
-                    content: "成功发表帖子！",//<a-result status="success" title=""/>,
-                    onOK(){
-                        this.destroyALL();
-                    }
-                })
+    handleCancel() {
+      this.postVisible = false;
     },
+    destroyALL() {
+      this.$destroyAll();
+    },
+    showSuccess() {
+      this.$success({
+        centered: true,
+        title: "发表成功",
+        content: "成功发表帖子！", //<a-result status="success" title=""/>,
+        onOK() {
+          this.destroyALL();
+        }
+      });
+    }
   }
 };
 </script>
--- src/frontend/src/views/Topic/Topic.vue
+++ src/frontend/src/views/Topic/Topic.vue
@@ -1,106 +1,118 @@
 <template>
   <div>
-  <a-layout style="min-height:100%">
-        <a-layout-content style="min-width:1200px;margin:0 auto;">
-            <a-page-header
-                style="margin-left:24px;margin-top:30px"
-                title="话题"
-                sub-title="Groups"
-            />
+    <a-layout style="min-height:100%">
+      <a-layout-content style="min-width:1200px;margin:0 auto;">
+        <a-page-header style="margin-left:24px;margin-top:30px" title="话题" sub-title="Groups" />
 
-            <div class="header">
-                <div>
-                    <h1 style="color:white;font-size:40px">#{{info.Topic_name}} </h1>
-                    <div style="color:white;margin:24px">话题简介：{{info.Topic_intro}}</div>
-                    <a-button style="text-align:center;border:1px" v-if="!ifJoinedTopic" type="default" @click="jointopic"><b>参与话题</b></a-button>
-                    <a-button v-if="ifJoinedTopic" type="default"><b>发表图文</b></a-button>
-                    <div style="margin-top:50px">
-                      <span style="color:white;margin:24px"><a-icon type="profile" style="margin:5px"/>话题相关：{{info.Topic_related}}</span>
-                    </div>
-                </div>
-            </div>
-
-            <!--发帖框，由postVisible控制-->
-            <a-modal  
-                centered="true"
-                title="发表图文"
-                :visible="postVisible"
-                :confirm-loading="confirmLoading"
-                @ok="handleOk"
-                @cancel="handleCancel"
-                okText="发表"
-                cancelText="取消"
-                >
-                <div>
-                        <a-input style="margin-top:10px" placeholder="请填写图文标题" />
-                        <a-textarea style="margin-top:10px" placeholder="请填写图文内容，图片可以点击下方上传按钮上传哦~" :auto-size="{ minRows: 5, maxRows: 5 }"></a-textarea>
-                        <div style="margin-top:10px">
-                          <a-upload
-                            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                            list-type="picture-card"
-                            :file-list="fileList"
-                            @preview="handlePreview"
-                            @change="handleChange"
-                          >
-                            <div v-if="fileList.length < 3"><!--最大上传三张图片-->
-                              <a-icon type="plus" />
-                              <div class="ant-upload-text">
-                                上传图片
-                              </div>
-                            </div>
-                          </a-upload>
-                          <a-modal :visible="previewVisible" :footer="null" @cancel="handleUCancel">
-                            <img alt="example" style="width: 100%" :src="previewImage" />
-                          </a-modal>
-                        </div>
-                </div>
-            </a-modal>
-
-            <a-divider>
-                <div style="font-size:18px">共有{{posts.length}}条图文</div>
-            </a-divider>
-
-            <!--帖子渲染部分-->
-            <a-list
-                class="comment-list"
-                item-layout="vertical"
-                :data-source="posts"
-                style="margin:24px;text-align:center"
+        <div class="header">
+          <div>
+            <h1 style="color:white;font-size:40px">#{{info.Topic_name}}</h1>
+            <div style="color:white;margin:24px">话题简介：{{info.Topic_intro}}</div>
+            <a-button
+              style="text-align:center;border:1px"
+              v-if="!ifJoinedTopic"
+              type="default"
+              @click="jointopic"
             >
-            <a-list-item slot="renderItem" slot-scope="item" style="text-align:left">
-                <a-list-item-meta :description="item.Topic_content_content">
-                  <a-avatar slot="avatar">{{item.User_name.substring(0,1)}}</a-avatar>
-                    <a slot="title" :href="'/object/'+item.Topic_content_id">
-                    {{item.Topic_content_title}}
-                    </a>
-                    <!--a-avatar slot="avatar" :src="item.avatar" /-->
-                </a-list-item-meta>
-                <template slot="actions" >
-                    <span> <a-icon type="user" style="margin-left: 8px" /> {{item.User_name}}</span>
-                    <a-tooltip :title="item.Create_time"><span>{{ item.Create_time}}</span></a-tooltip>    
-                </template>
-            </a-list-item>
-            </a-list>
-            
-        </a-layout-content>
+              <b>参与话题</b>
+            </a-button>
+            <a-button v-if="ifJoinedTopic" type="default">
+              <b>发表图文</b>
+            </a-button>
+            <div style="margin-top:50px">
+              <span style="color:white;margin:24px">
+                <a-icon type="profile" style="margin:5px" />
+                话题相关：{{info.Topic_related}}
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <!--发帖框，由postVisible控制-->
+        <a-modal
+          centered="true"
+          title="发表图文"
+          :visible="postVisible"
+          :confirm-loading="confirmLoading"
+          @ok="handleOk"
+          @cancel="handleCancel"
+          okText="发表"
+          cancelText="取消"
+        >
+          <div>
+            <a-input style="margin-top:10px" placeholder="请填写图文标题" />
+            <a-textarea
+              style="margin-top:10px"
+              placeholder="请填写图文内容，图片可以点击下方上传按钮上传哦~"
+              :auto-size="{ minRows: 5, maxRows: 5 }"
+            ></a-textarea>
+            <div style="margin-top:10px">
+              <a-upload
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                list-type="picture-card"
+                :file-list="fileList"
+                @preview="handlePreview"
+                @change="handleChange"
+              >
+                <div v-if="fileList.length < 3">
+                  <!--最大上传三张图片-->
+                  <a-icon type="plus" />
+                  <div class="ant-upload-text">上传图片</div>
+                </div>
+              </a-upload>
+              <a-modal :visible="previewVisible" :footer="null" @cancel="handleUCancel">
+                <img alt="example" style="width: 100%" :src="previewImage" />
+              </a-modal>
+            </div>
+          </div>
+        </a-modal>
+
+        <a-divider>
+          <div style="font-size:18px">共有{{posts.length}}条图文</div>
+        </a-divider>
+
+        <!--帖子渲染部分-->
+        <a-list
+          class="comment-list"
+          item-layout="vertical"
+          :data-source="posts"
+          style="margin:24px;text-align:center"
+        >
+          <a-list-item slot="renderItem" slot-scope="item" style="text-align:left">
+            <a-list-item-meta :description="item.Topic_content_content">
+              <a-avatar slot="avatar">{{item.User_name.substring(0,1)}}</a-avatar>
+              <a slot="title" :href="'/object/'+item.Topic_content_id">{{item.Topic_content_title}}</a>
+              <!--a-avatar slot="avatar" :src="item.avatar" /-->
+            </a-list-item-meta>
+            <template slot="actions">
+              <span>
+                <a-icon type="user" style="margin-left: 8px" />
+                {{item.User_name}}
+              </span>
+              <a-tooltip :title="item.Create_time">
+                <span>{{ item.Create_time}}</span>
+              </a-tooltip>
+            </template>
+          </a-list-item>
+        </a-list>
+      </a-layout-content>
     </a-layout>
   </div>
 </template>
 <style>
-.header{
-    height:400px;
-    margin:24px;
-    text-align:center;
-    background:rgb(62, 47, 146);
-    display: flex;
-    align-items: center;
-    justify-content: center;
+.header {
+  height: 400px;
+  margin: 24px;
+  text-align: center;
+  background: rgb(62, 47, 146);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
 <script>
-import global_ from '../../components/Global'
-import Vue from 'vue'
-
+import global_ from "../../components/Global";
+import Vue from 'vue';
 function getBase64(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -111,61 +123,74 @@ function getBase64(file) {
 }
 
 export default {
-  components:{
-    
-  },
-  created:function(){
-      this.id=this.$route.params.id;
-      console.log(this.id);
-      this.$http
+  components: {},
+  mounted: function() {
+    this.id = this.$route.params.id;
+    console.log(this.id);
+    this.$http
       .get("http://182.92.57.178:5000/topics/" + this.$route.params.id)
       .then(response => {
         this.info = response.data.result.info;
+        this.posts = response.data.result.contents;
         console.log(this.info);
       })
       .catch(response => {
         console.log(response);
       });
-      Vue.axios.post(//判断是否加入了小组？不知道可不可行
-          'http://182.92.57.178:5000/topics/'+this.id+'/join',
-          {
-          },
-          {
-            headers:{
-              'token':global_.token
-            }
-          }
-        ).then((res)=>{
-        console.log(res)
-      }).catch((res)=>{
-        this.ifJoinedTopic=true
-        console.log(res)
-      })
+    global_.my_topics.forEach(element => {
+      if (element.Topic_id == this.$route.params.id) {
+        this.ifJoinedTopic = true;
+      }
+    });
   },
   data() {
     return {
-        posts:[
-            {"Topic_content_id": 4, "Topic_content_content": "testtesttest", "Topic_content_title": "test", "Topic_id":1, 
-            "User_id": 15, "Username":"test","Topic_content_image": "/example", "Create_time": "2020-06-03 17:50:52", "Is_highlighted": 1, "Is_pinned": 1}, 
-            {"Topic_content_id": 5, "Topic_content_content": "testtesttest123", "Topic_content_title": "test123", "Topic_id": 1, 
-            "User_id": 15,  "Username":"test","Topic_content_image": "/example", "Create_time": "2020-06-03 18:30:04", "Is_highlighted": 1, "Is_pinned": 1}
-        ],
-        info:[],
-        postVisible:true,
-        ifJoinedTopic:false,
-        previewVisible: false,
-        previewImage: '',
-        fileList: [
+      posts: [],
+      info: [],
+      postVisible: true,
+      ifJoinedTopic: false,
+      previewVisible: false,
+      previewImage: "",
+      fileList: [
         {
-          uid: '-1',
-          name: 'image.png',
-          status: 'done',
-          url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-        },],
+          uid: "-1",
+          name: "image.png",
+          status: "done",
+          url:
+            "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+        }
+      ]
     };
   },
   methods: {
-    //估计得有一个getUsernameByID一个getPosts
+    jointopic() {
+      if (global_.loginStatus == false) {
+        alert("请先登录");
+        return;
+      }
+      console.log(global_.token);
+      Vue.axios
+        .post(
+          "http://182.92.57.178:5000/topics/" + this.id + "/join",
+          {},
+          {
+            headers: {
+              token: global_.token
+            }
+          }
+        )
+        .then(res => {
+          console.log(res);
+          this.ifJoinedTopic = true;
+          global_.my_topics.push({
+            Group_id: res.data.result.Topic_id,
+            User_id: res.data.result.User_id
+          });
+        })
+        .catch(res => {
+          console.log(res);
+        });
+    },
     //这几个是上传部分的函数
     handleUCancel() {
       this.previewVisible = false;
@@ -181,30 +206,30 @@ export default {
     handleChange({ fileList }) {
       this.fileList = fileList;
     },
-    showReport(){
-      this.postVisible=true;
-      },
-    handleOk(e){
+    showReport() {
+      this.postVisible = true;
+    },
+    handleOk(e) {
       console.log(e);
-      this.postVisible=false;
+      this.postVisible = false;
       this.showSuccess();
     },
-    handleCancel(){
-                this.postVisible=false;
-            },
-    destroyALL(){
-                this.$destroyAll();
-            },
-    showSuccess(){
-                this.$success({
-                    centered: true,
-                    title: '发表成功',
-                    content: "成功发表图文！",//<a-result status="success" title=""/>,
-                    onOK(){
-                        this.destroyALL();
-                    }
-                })
+    handleCancel() {
+      this.postVisible = false;
     },
-  },
+    destroyALL() {
+      this.$destroyAll();
+    },
+    showSuccess() {
+      this.$success({
+        centered: true,
+        title: "发表成功",
+        content: "成功发表图文！", //<a-result status="success" title=""/>,
+        onOK() {
+          this.destroyALL();
+        }
+      });
+    }
+  }
 };
 </script>
